9b18db07950cab595671f904ba6e9981da3dee40
Damon Dixon <ddixon176@gmail.com>
2026-03-19 22:16:30 -0400

    fixed api docs for https

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,11 @@
 from app import create_app
 from app.models import db
+from werkzeug.middleware.proxy_fix import ProxyFix 
 
 
 app = create_app('ProductionConfig')
 
-
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1)
 
 # creating the tables in the database
 with app.app_context():
